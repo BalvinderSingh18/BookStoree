ae44c1a71cea791c00432846f1b7278c85926b10
MangatVineforce <mangat.s@vineforce.net>
2025-06-04 14:59:32 +0530

    Implementation concept of TenantId and FullAuditedEntity

--- src/BookStore.Core/Books/Book.cs
+++ src/BookStore.Core/Books/Book.cs
@@ -1,15 +1,13 @@
 ﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BookStore.Books
 {
-    public class Book:Entity<int>
+    public class Book:FullAuditedEntity, IMayHaveTenant
     {
+        public int? TenantId { get; set; }
         [Required]
         public string Title { get; set; }
         [Required]
---
+++ src/BookStore.EntityFrameworkCore/Migrations/20250604092313_Update_Book_FullAuditedEntity_TenantId.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class Update_Book_FullAuditedEntity_TenantId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreationTime",
+                table: "Books",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatorUserId",
+                table: "Books",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "DeleterUserId",
+                table: "Books",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletionTime",
+                table: "Books",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "Books",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModificationTime",
+                table: "Books",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifierUserId",
+                table: "Books",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TenantId",
+                table: "Books",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreationTime",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CreatorUserId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DeleterUserId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "DeletionTime",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LastModificationTime",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifierUserId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "TenantId",
+                table: "Books");
+        }
+    }
+}
